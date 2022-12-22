25cc9293584ff04d425df98fc8c705ad90a29aa0
WalleMontero <montero@walle.mx>
2022-12-22 09:56:50 -0600

    Se añadio el ejercicio 10

---
+++ Ejercicio de Lógica de Programación 10/Ejercicio10.java
@@ -0,0 +1,71 @@
+package ejercicio10;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		// Creamos el diccionario Español-Inglés
+		HashMap<String, String> dictionary = new HashMap<>();
+		dictionary.put("casa", "house");
+		dictionary.put("perro", "dog");
+		dictionary.put("gato", "cat");
+		dictionary.put("mesa", "table");
+		dictionary.put("silla", "chair");
+		dictionary.put("libro", "book");
+		dictionary.put("lapicero", "pen");
+		dictionary.put("planta", "plant");
+		dictionary.put("soporte", "stand");
+		dictionary.put("ventana", "window");
+		dictionary.put("puerta", "door");
+		dictionary.put("ordenador", "computer");
+		dictionary.put("impresora", "printer");
+		dictionary.put("monitor", "monitor");
+		dictionary.put("ratón", "mouse");
+		dictionary.put("teclado", "keyboard");
+		dictionary.put("móvil", "mobile");
+		dictionary.put("television", "television");
+		dictionary.put("altavoz", "speaker");
+		dictionary.put("refrigerador", "refrigerator");
+
+		
+		
+		// Seleccionamos 5 palabras al azar del diccionario
+		Random random = new Random();
+		String[] words = new String[5];
+		
+		for (int i = 0; i < 5; i++) {
+		  // Obtenemos un índice aleatorio dentro del rango de índices del diccionario
+		  int index = random.nextInt(dictionary.size());
+		  
+		  // Seleccionamos la palabra correspondiente al índice
+		  String word = (String) dictionary.keySet().toArray()[index];
+		  
+		  //Añadimos la palabra al array de 5 palabras
+		  words[i] = word;
+		}
+
+		// Pedimos al usuario que traduzca las palabras
+		Scanner scanner = new Scanner(System.in);
+		int correctAnswers = 0;
+		int incorrectAnswers = 0;
+		
+		
+		for (String word : words) {
+		  // Mostramos la palabra al usuario y le pedimos que la traduzca al inglés
+		  System.out.println("Traduce la palabra '" + word + "' al inglés:");
+		  String answer = scanner.nextLine();
+		  // Comparamos la respuesta del usuario con la traducción correcta del diccionario
+		  if (answer.equals(dictionary.get(word))) {
+		    correctAnswers++;
+		  } else {
+		    incorrectAnswers++;
+		  }
+		}
+
+		// Mostramos cuántas respuestas correctas e incorrectas tiene el usuario
+		System.out.println("Tienes " + correctAnswers + " respuestas correctas y " + incorrectAnswers + " respuestas incorrectas.");
+	}
+
+}
